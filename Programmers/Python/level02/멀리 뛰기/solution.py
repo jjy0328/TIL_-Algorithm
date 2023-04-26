eb191b305506ab037f0b3d9439c6f305ee3ba83d
jjy0328 <wjdwndud3907@naver.com>
2023-04-26 22:50:10 +0900

    0426 algorithm

--- Programmers/Python/level02/멀리 뛰기/solution.py
+++ Programmers/Python/level02/멀리 뛰기/solution.py
@@ -6,3 +6,14 @@ def solution(n):
         return 2
     else:
         return solution(n - 1) + solution(n - 2)
+    
+
+# 다른 사람의 풀이
+def solution(n):
+    dp = [0]*(n+1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, n+1):
+        dp[i] = (dp[i-2] + dp[i-1])%1234567
+    return dp[n]
+#시간복잡도 = O(n), 공간복잡도 = O(n)
