f3c3decfd97789691a8bc9896c11d475ef640c7c
jjy0328 <wjdwndud3907@naver.com>
2023-05-17 19:26:06 +0900

    TIL Algorithm

--- Programmers/Python/Level01/가장 가까운 글자/solution.py
+++ Programmers/Python/Level01/가장 가까운 글자/solution.py
@@ -6,3 +6,19 @@ def solution(s):
         else:
             answer.append(-1)
     return answer
+
+
+####### 05/17일 풀이 : dictionary를 활용함 ###########
+def solution(s):
+    answer = []
+    dict_s = dict()
+
+    for i in range(len(s)):
+        if s[i] not in dict_s:
+            answer.append(-1)
+        else:
+            answer.append(i - dict_s[s[i]])
+        # 해당 요소가 가장 마지막에 나온 때로 업데이트를 해주어야 함
+        dict_s[s[i]] = i
+        
+    return answer
